216220c4161bf4374b4e7656f3d5b8d2227eb0cc
BenjaminAJ <ajanakub30@gmail.com>
2023-11-14 18:09:51 +0100

    Apply user validation middleware to user creation route

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const { getAllUsers, getUser, createUser } = require('../controller/user')
 const { loginUser } = require('../controller/auth')
-const { authenticateToken } = require('../middleware/verifyToken')
+const { authenticateToken } = require('../middleware/verifyToken');
+const validation = require('../middleware/validation');
+const { userValidationSchema } = require('../validation/validationSchemas');
 
 const UserRoute = express.Router();
 
 UserRoute.get('/users', authenticateToken ,getAllUsers)
 UserRoute.get('/user', authenticateToken ,getUser)
-UserRoute.post('/user', createUser)
+UserRoute.post('/user', validation(userValidationSchema) ,createUser)
 UserRoute.post('/login', loginUser)
 
 module.exports = UserRoute;
