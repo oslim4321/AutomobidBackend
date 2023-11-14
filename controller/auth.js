fe7bbed217b902d21957a44ca6f4abd0f75c3949
BenjaminAJ <ajanakub30@gmail.com>
2023-11-14 13:21:58 +0100

    Refactor login process in API endpoint for improved readability and error handling

--- controller/auth.js
+++ controller/auth.js
@@ -9,16 +9,9 @@ dotenv.config();
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user === null) {
-      return res.status(400).json({ message: "User not found" });
-    }
-    const passwordCheck = await bcrypt.compare(password, user.password);
-    if (passwordCheck) {
-      const accessToken = createToken(user.email, user._id)
-      return res.status(200).json({ accessToken });
-    }
-    res.status(401).json({ message: "Invalid credentials" });
+    const loginTask = await User.login(email, password)
+    const accessToken = createToken(email, loginTask._id)
+    res.status(200).json({accessToken})
   } catch (error) {
     const err = handleLoginErr(error);
     res.status(500).json(err);
