e97f98fb7f4e21f6c138ba7602100572d1875035
BenjaminAJ <ajanakub30@gmail.com>
2023-11-14 15:13:21 +0100

    Adjusted route names

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const { getAllUsers, getUserById, createUser } = require('../controller/user')
+const { getAllUsers, getUser, createUser } = require('../controller/user')
 const { loginUser } = require('../controller/auth')
 const { authenticateToken } = require('../middleware/verifyToken')
 
 const UserRoute = express.Router()
 
-UserRoute.get('/user', authenticateToken ,getAllUsers)
-UserRoute.get('/user/:id', authenticateToken ,getUserById)
+UserRoute.get('/users', authenticateToken ,getAllUsers)
+UserRoute.get('/user', authenticateToken ,getUser)
 UserRoute.post('/user', createUser)
 UserRoute.post('/login', loginUser)
 
