e9f8a478c1e19da591efea1896f108189178c6e5
BenjaminAJ <ajanakub30@gmail.com>
2023-11-14 12:05:36 +0100

    Return Access Token on Successful Login

--- controller/auth.js
+++ controller/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const { handleLoginErr } = require("../errors/custom-error");
+const { createToken } = require("./user");
 dotenv.config();
 
 const loginUser = async (req, res) => {
@@ -14,10 +15,7 @@ const loginUser = async (req, res) => {
     }
     const passwordCheck = await bcrypt.compare(password, user.password);
     if (passwordCheck) {
-      const accessToken = jwt.sign(
-        { email: user.email, id: user._id },
-        process.env.ACCESS_TOKEN_SECRET
-      );
+      const accessToken = createToken(user.email, user._id)
       return res.status(200).json({ accessToken });
     }
     res.status(401).json({ message: "Invalid credentials" });
