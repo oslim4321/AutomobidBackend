6a441cceba3886dea6e69edb7836209a7452bb7b
BenjaminAJ <ajanakub30@gmail.com>
2023-11-14 18:08:55 +0100

    Add user and subscriber validation schemas using Yup

---
+++ validation/validationSchemas.js
@@ -0,0 +1,13 @@
+const yup = require('yup')
+
+const userValidationSchema = yup.object({
+    userName: yup.string().required('Please provide username'),
+    email: yup.string().email('Please provide a valid email').required('Please provide email'),
+    password: yup.string().min(6, 'Password must be at least 6 character length').required('Please provide password')
+})
+
+const subscriberValidationSchema = yup.object({
+    email: yup.string().email('Please provide a valid email').required('Plese provide email')
+})
+
+module.exports = {userValidationSchema, subscriberValidationSchema}
