f66d2b803649a4049e67555ca40d6fd55bd352bc
BenjaminAJ <ajanakub30@gmail.com>
2023-11-14 08:30:17 +0100

    Handle Duplicate Email Error in Save User Details API

--- controller/subcription.js
+++ controller/subcription.js
@@ -11,6 +11,10 @@ const saveUserDetails = async (req, res) => {
     sendMail(user.email, subject, message); //send welcome mail to subscriber
     res.status(200).json({ success: true });
   } catch (error) {
+    if (error.code === 11000 && error.keyPattern && error.keyPattern.email) {
+      res.status(400).json({ message: 'Email already exists. Please use a different email address.' });
+    }
+    else
     res.status(500).json({ message: error.message });
   }
 };
