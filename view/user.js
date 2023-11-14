ff62c0cf489cb6c335021a8377f813c26491d993
BenjaminAJ <ajanakub30@gmail.com>
2023-11-14 11:15:20 +0100

    Updated API Endpoint for Routes

--- view/user.js
+++ view/user.js
@@ -3,11 +3,11 @@ const { getAllUsers, getUserById, createUser } = require('../controller/user')
 const { loginUser } = require('../controller/auth')
 const { authenticateToken } = require('../middleware/verifyToken')
 
-const router = express.Router()
+const UserRoute = express.Router()
 
 router.get('/user', authenticateToken ,getAllUsers)
 router.get('/user/:id', authenticateToken ,getUserById)
 router.post('/user', createUser)
 router.post('/login', loginUser)
 
-module.exports = router
+module.exports = UserRoute
