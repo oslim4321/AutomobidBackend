8bfffc319bd2995fd1a2f0df53dd42252fdd6424
BenjaminAJ <ajanakub30@gmail.com>
2023-11-15 16:28:46 +0100

    removed Mongoose connection configuration

--- db/connectDB.js
+++ db/connectDB.js
@@ -4,11 +4,7 @@ require('dotenv').config()
 module.exports.connectDB = async ()=>{
     try {
         const mongoUrl = process.env.MONGODB_URL;
-        await mongoose.connect(mongoUrl,   {
-            useNewUrlParser: true,
-            useCreateIndex: true,  // Use the new createIndex option
-            useUnifiedTopology: true,
-        })
+        await mongoose.connect(mongoUrl)
     } catch (error) {
         console.log(error.message);
         console.log('Could not connect to database');
