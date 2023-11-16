d75a70f443bee1393a91663c40afb82fc34c12f4
BenjaminAJ <ajanakub30@gmail.com>
2023-11-16 02:46:23 +0100

    Add login and user verification routes to UserRoute.

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,18 @@
 const express = require('express')
-const { getAllUsers, getUser, createUser } = require('../controller/user')
+const { getAllUsers, getUser, createUser, verifyUser, userHasbeenVerified, userHasNotbeenVerified } = require('../controller/user')
 const { loginUser } = require('../controller/auth')
 const { authenticateToken } = require('../middleware/verifyToken');
 const validation = require('../middleware/validation');
-const { userValidationSchema } = require('../validation/validationSchemas');
+const { userValidationSchema, userLoginSchema } = require('../validation/validationSchemas');
 
 const UserRoute = express.Router();
 
 UserRoute.get('/users', authenticateToken ,getAllUsers)
 UserRoute.get('/user', authenticateToken ,getUser)
 UserRoute.post('/user', validation(userValidationSchema) ,createUser)
-UserRoute.post('/login', loginUser)
+UserRoute.post('/login', validation(userLoginSchema),loginUser)
+UserRoute.get('/user/verify/:userId/:uniqueString', verifyUser)
+UserRoute.get('/user/verified', userHasbeenVerified)
+UserRoute.get('/user/verified/error', userHasNotbeenVerified)
 
 module.exports = UserRoute;
