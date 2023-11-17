d966464e0e0577dbd030ada766abde92b1ab6d70
BenjaminAJ <ajanakub30@gmail.com>
2023-11-17 05:28:41 +0100

    Implement POST endpoint for user password reset functionality in UserRoute

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { getAllUsers, getUser, createUser, verifyUser, userHasbeenVerified, userHasNotbeenVerified } = require('../controller/user')
-const { loginUser, resetUserPassword } = require('../controller/auth')
+const { loginUser, resetUserPassword, actuallyResetUserPassword } = require('../controller/auth')
 const { authenticateToken } = require('../middleware/verifyToken');
 const validation = require('../middleware/validation');
 const { userValidationSchema, userLoginSchema } = require('../validation/validationSchemas');
@@ -15,5 +15,6 @@ UserRoute.get('/user/verify/:userId/:uniqueString', verifyUser)
 UserRoute.get('/user/verified', userHasbeenVerified)
 UserRoute.get('/user/verified/error', userHasNotbeenVerified)
 UserRoute.post('/user/requestPasswordReset', resetUserPassword)
+UserRoute.post('/user/resetPassword', actuallyResetUserPassword)
 
 module.exports = UserRoute;
